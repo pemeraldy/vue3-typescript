7e8e8021af2b2ce7205d17f9f7728aa22e549c5f
pemeraldy <pemeraldy@gmail.com>
2021-04-09 02:01:13 +0100

    chapter two:item component and lists with proptypes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <h1>Journey into vue3</h1>
+    <div id="nav" class="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </div>
   <router-view/>
 </template>
-
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent ({
+  name: 'App'
+})
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    h2 {
+      margin: 0;
     }
   }
-}
+
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
---
+++ src/components/items/ItemList.component.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>Item Lists</h3>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{item.name}}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent ({
+  props : {
+    items : {
+      type: Array as PropType<any[]>
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
